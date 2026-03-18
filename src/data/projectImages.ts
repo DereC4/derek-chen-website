9d9c6f9fd5f9106b749603a48eab118032b97fe1
Derek <derex1987@gmail.com>
2026-03-17 21:16:04 -0500

    feat: two more projects registered

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -3,9 +3,13 @@ import type { StaticImageData } from 'next/image';
 import slorkWars1 from '@/assets/pics/projects/slorkwars-1.png';
 import slorkWars2 from '@/assets/pics/projects/slorkwars-2.png';
 import guests1 from '@/assets/pics/projects/guestsplugin.png'
+import utgradedist from '@/assets/pics/projects/ut-grade-distribution-viewer.png'
+import utregplus from '@/assets/pics/projects/ut-registration-plus-1.png'
 
 export const projectImages: Record<string, StaticImageData> = {
     'slork-wars': slorkWars1,
     'slork-wars-2': slorkWars2,
-    'guests': guests1
+    'guests': guests1,
+    'ut-registration-plus': utgradedist,
+    'ut-grade-distribution-viewer': utregplus,
 };
