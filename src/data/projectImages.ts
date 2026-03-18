9784fcfb8b2257080bff23c5ac0bcc116d14277d
Derek <derex1987@gmail.com>
2026-03-17 20:48:23 -0500

    feat: more project images and readme

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -1,6 +1,11 @@
 import type { StaticImageData } from 'next/image';
-import slorkWars from '@/assets/pics/projects/slorkwars-1.png';
+
+import slorkWars1 from '@/assets/pics/projects/slorkwars-1.png';
+import slorkWars2 from '@/assets/pics/projects/slorkwars-2.png';
+import guests1 from '@/assets/pics/projects/guestsplugin.png'
 
 export const projectImages: Record<string, StaticImageData> = {
-    'slork-wars': slorkWars,
+    'slork-wars': slorkWars1,
+    'slork-wars-2': slorkWars2,
+    'guests': guests1
 };
