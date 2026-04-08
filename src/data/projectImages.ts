6c63e7c26ced5d26a394fd6fb11afe27660ef956
Derek <derex1987@gmail.com>
2026-04-08 00:01:58 -0500

    chore: register project pic

--- src/data/projectImages.ts
+++ src/data/projectImages.ts
@@ -5,6 +5,7 @@ import slorkWars2 from '@/assets/pics/projects/slorkwars-2.png';
 import guests1 from '@/assets/pics/projects/guestsplugin.png';
 import utgradedist from '@/assets/pics/projects/ut-grade-distribution-viewer.png';
 import utregplus from '@/assets/pics/projects/ut-registration-plus-1.png';
+import utregplus2 from '@/assets/pics/projects/ut-registration-plus-2.png';
 import pearDesktop1 from '@/assets/pics/projects/youtube-music-1.png';
 import pearDesktop2 from '@/assets/pics/projects/youtube-music-2.png';
 import pearDesktop3 from '@/assets/pics/projects/youtube-music-3.png';
@@ -27,7 +28,8 @@ export const projectImages: Record<string, StaticImageData> = {
     'pear-desktop-4': pearDesktop4,
     'pear-desktop-5': pearDesktop5,
     // 3.17.2026 ai did something clever here and found out i accidentally swapped the two, that is crazy, 2023 chatgpt would've never
-    'ut-registration-plus': utregplus,
+    'ut-registration-plus': utregplus2,
+    'ut-registration-plus-2': utregplus,
     'ut-distribution-viewer': utgradedist,
     accountech: accountech,
     trove: trove,
